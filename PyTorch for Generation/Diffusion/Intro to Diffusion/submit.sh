455f60f7373f83b5fc25501f73037634e1e92524
Priyam Mazumdar <60265010+priyammaz@users.noreply.github.com>
2024-07-29 14:24:36 -0500

    Update submit.sh

--- PyTorch for Generation/Diffusion/Intro to Diffusion/submit.sh
+++ PyTorch for Generation/Diffusion/Intro to Diffusion/submit.sh
@@ -1,11 +1,11 @@
 accelerate launch train.py --experiment_name "CELEBA_DIFFUSION" \
-                           --path_to_data "/scratch/bbug/reu/data/celeba/" \
-                           --working_directory "/u/priyamm2/HAL-DL-From-Scratch/PyTorch for Generation/Diffusion/Intro to Diffusion/work_dir" \
-                           --generated_directory "/u/priyamm2/HAL-DL-From-Scratch/PyTorch for Generation/Diffusion/Intro to Diffusion/generated" \
+                           --path_to_data "<PATH_TO_YOUR_DATA>" \
+                           --working_directory "<PATH_TO_YOUR_WORKING_DIRECTORY>" \
+                           --generated_directory "<PATH_TO_YOUR_GENERATION_DIRECTORY>" \
                            --num_diffusion_timesteps 1000 \
                            --plot_freq_interval 100 \
                            --num_generations 3 \
-                           --num_training_steps 200000 \
+                           --num_training_steps 250000 \
                            --warmup_steps 2000 \
                            --evaluation_interval 500 \
                            --batch_size 64 \
